6ecf320c01f3040cd7f7f3a1333bdfc78ca38607
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2016-01-29 17:28:53 +0100

    Update metadata.rb

--- chef/site-cookbooks/sample_cb/metadata.rb
+++ chef/site-cookbooks/sample_cb/metadata.rb
@@ -2,7 +2,7 @@ name             'sample_cb'
 maintainer       'Sirajudheen'
 maintainer_email 'sirajudheen.mohamed.ali@sap.com'
 license          'All rights reserved'
-description      'Installs/Configures chef-sample'
+description      'Installs/Configures sample_cb'
 long_description <<-TEXT
     Very simple recipe, converge a directory under /tmp
 TEXT
