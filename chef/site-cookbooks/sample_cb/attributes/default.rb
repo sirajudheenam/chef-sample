d5f90173f294a369721f7ce484026e96adbfd0c9
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2016-07-13 17:02:54 +0200

    Update default.rb

--- chef/site-cookbooks/sample_cb/attributes/default.rb
+++ chef/site-cookbooks/sample_cb/attributes/default.rb
@@ -1,3 +1,3 @@
 default[:sample_cb][:the_dirname] = '/tmp/newly_created_directory'
-default[:sample_cb][:the_owner]   = 'i072278'
+default[:sample_cb][:the_owner]   = 'sam'
 default[:sample_cb][:the_perms]   = '0775'
